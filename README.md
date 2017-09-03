eca435f6d54243ad9776816d2c5ac1da422a52e7
Erik Swanson <theerikswanson@gmail.com>
2017-09-02 22:28:24 -0400

    Add pbcopy and pbpaste aliases, guarded by xclip check

--- README.md
+++ README.md
@@ -13,4 +13,6 @@ $ ./install.sh
 from within this directory. Running `install.sh` is set up to be
 idempotent. Running it multiple times should have no effect. Running
 it after the repository has been updated should update the dotfiles
-without breaking things.
+without breaking things. Additionally, it should not choke if certain
+functionality is not installed, but should print out a message saying
+to install it.
--- bash/aliases.sh
+++ bash/aliases.sh
@@ -21,3 +21,11 @@ alias l='ls -CF'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Make a pbcopy alias if xclip is around
+if command -v xclip >/dev/null 2>&1; then
+    alias pbcopy='xclip -selection clipboard'
+    alias pbpaste='xclip -selection clipboard -o'
+else
+    echo "[WARN] xclip -- not installed"
+fi
